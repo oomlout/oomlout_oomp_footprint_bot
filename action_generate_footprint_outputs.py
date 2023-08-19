141cc08b8d5d9d27df9d364ada33b1324b7fbb22
Oom Lout <aaron@oomlout.com>
2023-08-19 07:07:48 +0100

    comitting after 250 generations

--- action_generate_footprint_outputs.py
+++ action_generate_footprint_outputs.py
@@ -1,18 +1,24 @@
 import oom_kicad
 import os
+import oomBase
 
 def go_through_directories():
+    oomBase.oomSendAltTab(delay=5)
     count = 1
     count2 = 1
     # go througyh all directories in footprints
     for root, dirs, files in os.walk("footprints"):
         #for each directory
         for name in dirs:
-            #go through the files in this directory just one level
             for file in os.listdir(os.path.join(root, name)):
                 #if kicad_mod file
+
+                ######## testing
+                #file = "working.kicad_mod"
                 if file.endswith(".kicad_mod"):
                     filename = os.path.join(root, name, file)
+                    #test
+                    #filename = "footprints/alexisvl_ipc7351_least_qfn50p900x900x100_65c/working/working.kicad_mod"
                     #filter = "kicayyyyyC:\GH\oomlout_oomp_footprint_bot\footprints\alexisvl_ipc7351_least_qfn50p800x800x80_49w\working\working.pdf
                     # yd_libraries_kicad"
                     filter = ""
@@ -20,15 +26,15 @@ def go_through_directories():
                     
                     # yr = "iangitpers"
                     if filter in filename.lower():
-                        print("footprint output generating for: " + filename)
-                        counter = oom_kicad.generate_outputs_footprint(filename=filename)
+                        #print("footprint output generating for: " + filename)
+                        counter = oom_kicad.generate_outputs_footprint(filename=filename, computer="surface")
                         count += counter
                         #push to git using oom_kicad every 100 files
                         if count % 250 == 0:
                             oom_kicad.push_to_git(count=count)
                             count = count + 1
             count2 = count2 + 1
-            if count % 5000 == 0:
+            if count2 % 100 == 0:
                 #print a dot
                 print(".", end="", flush=True)
     oom_kicad.push_to_git(count=count)
