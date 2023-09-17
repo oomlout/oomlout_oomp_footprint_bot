c376dc214071dc381a721b2f88c095440580e5c7
Oom Lout <aaron@oomlout.com>
2023-09-17 16:40:19 +0100

    comitting after 1 generations

---
+++ action_all_all.py
@@ -0,0 +1,41 @@
+import tmp.data.oomlout_oomp_footprint_src.oomlout_oomp_footprint_src as oom_f_s
+
+
+import oom_git
+
+
+def main(**kwargs):
+    import time
+    #time_start 
+    test=False
+    dir_src = "tmp/data/"
+    src_github = "https://github.com/oomlout/oomlout_oomp_footprint_src"
+    oom_git.clone(repo = src_github, directory=dir_src)    
+    dir_src = f"{dir_src}/oomlout_oomp_footprint_src"
+    
+    directory = dir_src
+
+    #make the repo.yaml file not really needed very often
+    oom_f_s.make_footprint_yaml(directory=directory, test=test)
+
+    oom_f_s.clone_and_copy_footprints(directory=directory, test=test)
+   
+    #oom_f_s.make_footprints_readme()
+
+    #push footprint_src    
+    oom_git.push_to_git(directory=directory)
+
+
+
+    #oomlout_oomp_symbol_src.clone_and_copy_symbols(test=test, dir_base="tmp/data/oomlout_oomp_symbol_src")
+    #oomlout_oomp_symbol_src.make_symbols_readme()
+    #dir_all_symbols = "tmp/data/oomlout_oomp_symbol_all_the_kicad_symbols"
+    #oom_git.push_to_git(directory=dir_all_symbols)
+    #action_setup.main()
+    #action_generate_symbol_outputs.main()
+    #action_generate_readmes.main()
+    #action_generate_image_resolutions.main()
+
+
+if __name__ == '__main__':
+    main()
